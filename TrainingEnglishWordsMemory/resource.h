b20104a7b6b31e69ba6eec17eaaf617b7c5e7f55
Hatter <57457604+SashaZemC@users.noreply.github.com>
2024-06-19 21:54:40 +0300

    reuploaded to change folder name

---
+++ TrainingEnglishWordsMemory/resource.h
@@ -0,0 +1,52 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by TrainingEnglishWordsMemorybyZemtsov.rc
+//
+#define IDD_TRAININGENGLISHWORDSMEMORY_BY_ZEMTSOV_DIALOG 102
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP2                     131
+#define IDB_BITMAP3                     133
+#define IDR_MENU1                       134
+#define IDD_SHORTGUIDE                  137
+#define IDD_ABOUTAPPLICATION            140
+#define IDD_ERRORWINDOW                 141
+#define IDD_AUTHORS                     142
+#define IDD_TRAINING                    143
+#define IDR_MENU2                       146
+#define IDD_SETTINGS                    147
+#define IDD_CHANGEFILE                  148
+#define IDB_BITMAP4                     149
+#define IDB_BITMAP5                     150
+#define IDB_BITMAP1                     151
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1001
+#define IDC_E1                          1001
+#define IDC_EDIT1                       1002
+#define IDC_E2                          1003
+#define IDC_EDIT2                       1003
+#define IDC_COMBO1                      1005
+#define IDC_E3                          1006
+#define IDC_CHECK1                      1007
+#define IDC_CHECK2                      1008
+#define IDC_CHECK3                      1009
+#define IDC_CHECK4                      1010
+#define ID_ABOUT_SHORTGUIDE             32771
+#define ID_ABOUT_ABOUTAPPLICATION       32772
+#define ID_ABOUT_AUTHORS                32773
+#define ID_ABOUT_SENDFEEDBACK           32774
+#define ID_MENU_START                   32775
+#define ID_MENU_STOP                    32776
+#define ID_MENU_RESET                   32777
+#define ID_MENU_SETTINGS                32778
+#define ID_MENU_CHOOSEFILENAME          32779
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        152
+#define _APS_NEXT_COMMAND_VALUE         32780
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
