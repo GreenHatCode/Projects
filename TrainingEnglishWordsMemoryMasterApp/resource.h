a73ac306f4a66e321c314b819c10ae47e644b0bc
Hatter <57457604+SashaZemC@users.noreply.github.com>
2024-06-19 21:53:30 +0300

    reuploaded to change folder name

---
+++ TrainingEnglishWordsMemoryMasterApp/resource.h
@@ -0,0 +1,37 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by TrainingEnglishWordsMemoryMasterApp20.rc
+//
+#define IDD_TRAININGENGLISHWORDSMEMORYMASTERAPP20_DIALOG 102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       130
+#define IDD_SHORTGUIDE                  131
+#define IDD_ABOUTAPPLICATION            132
+#define IDB_BITMAP1                     133
+#define IDD_ERRORWINDOW                 133
+#define IDB_BITMAP2                     134
+#define IDD_ABOUTAPPLICATION1           134
+#define IDD_AUTHORS                     134
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
+#define IDC_BUTTON1                     1002
+#define IDC_PIC                         1003
+#define IDC_E2                          1004
+#define ID_ABOUT_SHORTGUIDE             32771
+#define ID_ABOUT_AUTHORS                32772
+#define ID_ABOUT_ABOUTAPPLICATION       32773
+#define ID_ABOUT_SENDFEEKBACK           32774
+#define ID_FILE_OPEN32775               32775
+#define ID_FILE_CLOSE32776              32776
+#define ID_SAVECHANGES                  32777
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_COMMAND_VALUE         32778
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
